b1e8e2b971321b6a14f02b4d4cafea8e26a005a5
Juan Medin <jmedin1965@gmail.com>
2016-07-05 23:55:30 +1000

    Changes to be committed: 	new file:   git-check.sh 	modified:   git-pull-local.sh
    check git repo at / and send email if changed
    updated git-pull to work for ipfire as well

--- sbin/git-pull-local.sh
+++ sbin/git-pull-local.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-cd /usr/local
-[ -d .git ] || (echo "/usr/log: not a git repo"; exit 1)
-
+hostname="$(/bin/hostname)"
 prog="/usr/bin/git pull"
 log=""
 sucessMsg="Already up-to-date."
-mailSubject="${prog##*/} for /usr/local from $(/bin/hostname)"
+mailSubject="${prog##*/} for /usr/local from $hostname"
 mailTo="root"
+
+case "$hostname" in
+"ipfire")
+	cd /root/local;;
+*)
+	cd /usr/local;;
+esac
+
+pwd="$(pwd)"
+
+[ -d .git ] || (echo "$pwd: not a git repo"; exit 1)
 
 msg="$($prog 2>&1; echo ;echo EV = $?)"
 [ -n "$log" ] && echo "$msg" >> "$log"
 if [ "$(echo "$msg" | /bin/fgrep -c "$sucessMsg")" -eq 0 ]
 then
-	echo "$msg" | /usr/bin/mail -s "$mailSubject" $mailTo
+	if [ -x /usr/bin/mail ]
+	then
+		echo "$msg" | /usr/bin/mail -s "$mailSubject" $mailTo
+		
+	elif [ -x /usr/local/bin/sendEmail ]
+	then
+		echo "$msg" | /usr/local/bin/sendEmail \
+			-f root@ipfire.jmsh-home.dtdns.net \
+			-t jmedin@joy.com \
+			-s smtp.dodo.com \
+			-u "$mailSubject"
+	fi
 fi
-
