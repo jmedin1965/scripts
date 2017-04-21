8701769b067848a8002751fdc3ef02d87071a53b
Juan Medin <jmedin1965@gmail.com>
2017-04-22 00:34:50 +1000

    added check to ignore /etc/hosts if it not different. This is due to gitattributes filer

--- sbin/git-check.sh
+++ sbin/git-check.sh
@@ -1,5 +1,8 @@
 
 cd /
+git add /etc/letsencrypt/*  2>&1 /dev/null
+git commit -m "letsencript auto commit" 2>&1 /dev/null
+[ "$(git diff /etc/hosts | wc -l)" == 0 ] && git add /etc/hosts
 /usr/bin/git status > /tmp/git-check.txt 2>&1
 #echo "$(/bin/fgrep -c "nothing to commit, working directory clean" /tmp/git-check.txt)"
 if [ "$(/bin/fgrep -c "nothing to commit, working directory clean" /tmp/git-check.txt)" == 0 ]
