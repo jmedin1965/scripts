dde49d57bc1888286b5250d90d948c937d030ab5
Foreman Root <root@jmsh-home.com>
2019-01-08 16:54:13 +1100

    update: bin/add-git-scripts.sh

--- bin/add-git-scripts.sh
+++ bin/add-git-scripts.sh
@@ -6,11 +6,16 @@ if [ ! -d /opt/git-repo/local/.git ]; then
 
 elif [ -d /usr/local/scripts ]; then
 	echo "/usr/local/scripts/.git: folder already exist"
-	exit 1
 
 else
 	cd /usr/local
 	git clone /opt/git-repo/local scripts
 fi
 
+cd /usr/local/scripts
+git pull
 
+if [ -e /usr/local/scripts/sbin/usr-local-path.sh -a ! -e /etc/profile.d/usr-local-path.sh ]
+then
+	ln -s /usr/local/scripts/sbin/usr-local-path.sh /etc/profile.d/usr-local-path.sh
+fi
