772cbb1020ce9e4ecb35c99d932311c81a65e661
Foreman Root <root@jmsh-home.com>
2020-01-11 12:17:08 +1100

    fix: added -y option to lvremove for  fix-home

--- sbin/fix-home-mnt.sh
+++ sbin/fix-home-mnt.sh
@@ -4,7 +4,7 @@
 if [ "$(mount | fgrep " /home " | fgrep -c /dev/mapper/)" -gt 0 ]
 then
 	umount /home
-	lvremove /dev/vg00/home
+	lvremove -y /dev/vg00/home
 	lvresize -l 100%FREE /dev/vg00/root
 	resize2fs /dev/vg00/root
 	cp /etc/fstab /etc/fstab.old
