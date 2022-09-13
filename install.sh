0d04d081e16593d47207fb62938719df3133f541
Foreman Root <root@jmsh-home.com>
2022-09-13 17:16:57 +1000

    added: vimrc.local to install and fixed vimrc.local

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 git_repo_local="/usr/local/scripts"
 git_repo="https://jmedin1965@github.com/jmedin1965/scripts.git"
 
-# clone the repo
+# clone the repo if it does not exest, else git pull
 if [ -d "${git_repo_local}/.git" ]
 then
     cd "${git_repo_local}"
@@ -26,6 +26,19 @@ cron_d=""
 [ -d "/etc/fcron.daily" ] && cron_d="/etc/fcron.daily"
 [ -d "/etc/cron.daily" ]  && cron_d="/etc/cron.daily"
 [ -n "${cron_d}" ] && /bin/ln -fs "$git_repo_local/install.sh" "${cron_d}/usr-local-scripts.sh"
+
+vimlocal="/etc/vim/vimrc.local"
+# do vim fixes
+#
+# if file exists and is a real file or if it's a link and not pointing to the scripts version
+if [ -e "$vimlocal" -a ! -h "$vimlocal" ] || [ -h "$vimlocal" -a "$(/bin/readlink -f "$vimlocal")" != "$git_repo_local$vimlocal" ]
+then
+    /bin/rm -f "$vimlocal"
+fi
+if [ ! -e "$vimlocal" ]
+then
+    /bin/ln -s "$git_repo_local$vimlocal" "$vimlocal"
+fi
 
 # do cleanup
 unset git_repo git_repo_local cron_d
