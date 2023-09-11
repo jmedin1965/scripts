2d4367198b4f26d3a3bb5f2ec903f29eb681142a
root <root@mvm001vrs.jgi.joyglobalinc.com>
2023-09-11 00:38:42 +0000

    add: mariadb stuff to add bareos client

--- sbin/bareos-add-client.sh
+++ sbin/bareos-add-client.sh
@@ -16,6 +16,18 @@ err()
     msg "$@" > /dev/stderr
 }
 
+is_installed()
+{
+    if [ -n "$1" -a -n "$2" ] 
+    then
+        [ "$(/usr/bin/ssh $1 -x "/usr/bin/apt list --installed | /usr/bin/fgrep "$2" -c")" -gt 0 ] && return 0 
+    else
+        return 2
+    fi
+
+    return 1
+}
+
 #
 # agp random password generator
 #
@@ -51,6 +63,16 @@ else
 			/usr/bin/ssh $1 -x "/usr/bin/apt -y upgrade"
 			/usr/bin/ssh $1 -x "/usr/bin/apt -y install bareos-filedaemon"
 
+            # install maria DB Backup and bareos mariadbbackup plugin
+            # REF: https://docs.bareos.org/TasksAndConcepts/Plugins.html#storage-daemon-plugins 
+            # REF: https://docs.bareos.org/Appendix/Howtos.html
+            if is_installed "$1" mariadb-server
+            then
+                /usr/bin/ssh $1 -x "/usr/bin/apt -y install mariadb-backup bareos-filedaemon-mariabackup-python-plugin"
+                /usr/bin/ssh $1 -x "/bin/cp -f /etc/bareos/bareos-fd.d/client/myself.conf /etc/bareos/bareos-fd.d/client/myself.conf.bak"
+                /usr/bin/ssh $1 -x "/bin/sed -e 's/\(\s*\)#\s*\(Plugin Directory\s*=\)/\1\2/g' -e 's/\(\s*\)#\s*\(Plugin Names\s*=\).*/\1\2 \"python3\"/g' /etc/bareos/bareos-fd.d/client/myself.conf.bak > /etc/bareos/bareos-fd.d/client/myself.conf"
+            fi
+
             # the client details
 			name="$(/usr/bin/ssh $1 -x /usr/bin/hostname --short)-fd"
 			echo name=$name
@@ -61,7 +83,7 @@ else
 
             if [ -e "/etc/bareos/bareos-dir.d/client/$name.conf" ]
             then
-                msg "client file for $name exists. has this client already been added?"
+                msg "client file for $name exists. Has this client already been added?"
             else
                 # add client and extract client file for copying
 			    exp_file=$(echo "configure add client name=$name address=$ip password=$pw" | /usr/bin/sudo -u bareos bconsole)
