a7078167662c2383f2b4b2cdea3c341fb5fed484
Juan Medin <jmedin1965@gmail.com>
2024-05-29 13:49:36 +1000

    fix: ssh-agent.sh consider .back_logout now too and cleans up

--- profile.d/ssh-agent.sh
+++ profile.d/ssh-agent.sh
@@ -1,7 +1,46 @@
 
 AUTH_SOCK=~/.ssh/ssh-agent.sock
+AUTH_SOCK_LINK=~/.ssh/ssh-agent-link.sock
+AUTH_SOCK_D=~/.ssh/ssh-agent.sock.d
 SSH_AGENT="ssh-agent"
-AUTH_SOCK_ORIG=""
+
+if [ "$1" == logout ]
+then
+	echo "Loggout: SSH_AUTH_SOCK_ORIG=$SSH_AUTH_SOCK_ORIG"
+	if [ -n "$SSH_AUTH_SOCK_ORIG" ]
+	then
+		echo "remove SSH_AUTH_SOCK that this session was using $SSH_AUTH_SOCK_ORIG"
+		echo /bin/rm -f "$SSH_AUTH_SOCK_ORIG"
+        SSH_AUTH_SOCK=""
+        for SSH_AUTH_SOCK_ORIG in $(/bin/ls -c --reverse "$AUTH_SOCK_D"/*)
+		do
+			if [ -S "$SSH_AUTH_SOCK_ORIG" ]
+			then
+                if [ -z "$SSH_AUTH_SOCK" ]
+                then
+                    SSH_AUTH_SOCK="$SSH_AUTH_SOCK_ORIG"
+                    echo "testing: $SSH_AUTH_SOCK"
+                    ssh-add -l 2>/dev/null >/dev/null   # test if local socket is active
+                    if [ $? -ge 2 ] # if not active
+                    then
+                        echo " loop: /bin/rm -f \"$SSH_AUTH_SOCK_ORIG\""
+                        /bin/rm -f "$SSH_AUTH_SOCK_ORIG"
+                    else
+                        echo " loop: ln -sf \"$SSH_AUTH_SOCK_ORIG\" \"$AUTH_SOCK_LINK\""
+                        /bin/ln -sf "$SSH_AUTH_SOCK_ORIG" "$AUTH_SOCK_LINK"
+                    fi
+                fi
+			else
+				echo " loop: /bin/rm -f \"$SSH_AUTH_SOCK_ORIG\""
+				/bin/rm -f "$SSH_AUTH_SOCK_ORIG"
+			fi
+		done
+	fi
+    echo done
+	exit 0
+fi
+
+export SSH_AUTH_SOCK_ORIG=""
 
 # Cygwin ssh-agent socket
 if [ "$(/bin/uname -o)" == "Cygwin" ]
@@ -14,6 +53,7 @@ fi
 # if we have been pased an SSH_AUTH_SOCK then
 if [ -n "$SSH_AUTH_SOCK" ]
 then
+    [ -d "$AUTH_SOCK_D" ] || ( /bin/mkdir "$AUTH_SOCK_D"; /bin/chmod 0700 "$AUTH_SOCK_D" )
     echo "msg: SSH_AUTH_SOCK was set to: $SSH_AUTH_SOCK"
     if [ "$SSH_AUTH_SOCK" != "$AUTH_SOCK" ] # if SSH_AUTH_SOCK is not pointing to the local one then
     then
@@ -26,8 +66,19 @@ then
             export SSH_AUTH_SOCK="$AUTH_SOCK"
         else
             echo "msg: $SSH_AUTH_SOCK is active, use it."
-            echo ln -sf "$SSH_AUTH_SOCK" "$AUTH_SOCK"
-            ln -sf "$SSH_AUTH_SOCK" "$AUTH_SOCK"
+
+            SSH_AUTH_SOCK_ORIG="$AUTH_SOCK_D/`/bin/basename "$SSH_AUTH_SOCK"`"
+            echo ln -sf "$SSH_AUTH_SOCK" "$SSH_AUTH_SOCK_ORIG"
+            ln -sf "$SSH_AUTH_SOCK" "$SSH_AUTH_SOCK_ORIG"
+
+            echo ln -sf "$SSH_AUTH_SOCK_ORIG" "$AUTH_SOCK_LINK"
+            ln -sf "$SSH_AUTH_SOCK_ORIG" "$AUTH_SOCK_LINK"
+            
+            export SSH_AUTH_SOCK="$AUTH_SOCK_LINK"
+
+            echo SSH_AUTH_SOCK=$SSH_AUTH_SOCK
+            echo SSH_AUTH_SOCK_ORIG=$SSH_AUTH_SOCK_ORIG
+            export SSH_AUTH_SOCK_ORIG
         fi
     fi
 else
@@ -46,5 +97,15 @@ then
     echo "$SSH_AGENT: started on: $SSH_AUTH_SOCK"
 else
     echo "msg: agent already active, nothing to be done."
+fi
+
+unset AUTH_SOCK
+unset AUTH_SOCK_LINK
+unset UTH_SOCK_D
+unset SSH_AGENT
+
+if [ -e "/etc/profile.d/ssh-agent.sh" -a "$(/bin/fgrep -c "/bin/bash /etc/profile.d/ssh-agent.sh logout" ~/.bash_logout)" == 0 ]
+then
+    echo "/bin/bash /etc/profile.d/ssh-agent.sh logout" >> ~/.bash_logout
 fi
 
