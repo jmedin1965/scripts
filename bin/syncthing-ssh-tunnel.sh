4b6e35550a9eadf6f642d3219d5827e85542fb0b
Juan Medin <jmedin1965@gmail.com>
2026-02-08 21:43:57 +1100

    fix: syncthing-ssh-tunnel.sh, can now add and only starts one chrome browser too.

--- bin/syncthing-ssh-tunnel.sh
+++ bin/syncthing-ssh-tunnel.sh
@@ -1,58 +1,85 @@
 #!/bin/bash
-#
+
+
 port="8384"
+TUN_PID=""
+BROWSER_PID=""
 
-if [ -z "$1" ]
-then
-    echo Usage $(basename "$0") remote-host
-    exit 1
-fi
-
-echo port $port from host $1 will be available locally
-
-/mnt/c/Program\ Files/WSL/wslg.exe -d Ubuntu-24.04 --cd "~" -- /snap/bin/chromium https://localhost:$port
-
-ssh -L 8384:localhost:8384 -J jmedin@192.168.10.127 root@$1 -N
-
-#!/bin/bash
+# a function to trap CTRL_C since ssh-add times out or waits forever
+ctrl_c() {
+    echo "** Trapped CTRL-C"
+    [ -n "$TUN_PID" ] && kill $TUN_PID
+    echo "msg: trap done"
+}
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
 
 main()
 {
-    occ=""
-    [ -e /var/www/nextcloud/occ ] && occ="/var/www/nextcloud/occ"
-    [ -e /app/www/public/occ ]    && occ="/app/www/public/occ"
+    while :
+    do
+        if [ $# == "0" ]
+        then
+            echo -n "Connect to which host, or 'q', to quit: "
+            read result
+            set -- "$result"
 
-    updater_phar=""
-    [ -e /var/www/nextcloud/updater/updater.phar ] && updater_phar="updater.phar"
-    [ -e /app/www/public/updater/updater.phar ]    && updater_phar="/app/www/public/updater/updater.phar"
+        elif [ $# == 1 -a -z "$TUN_PID" ]
+        then
+            result="$1"
+        else
+            menu "$@"
+        fi
 
-    conf_f=""
-    [ -e "/var/www/nextcloud/config/config.php" ]         && conf_f="/var/www/nextcloud/config/config.php"
-    [ -e "/data/config/www/nextcloud/config/config.php" ] && conf_f="/data/config/www/nextcloud/config/config.php"
-    
-    user="$(stat -c "%U" "$occ")"
+        if [ "$result" == "q" ]
+        then
+            echo $TUN_PID=$TUN_PID
+            [ -n "$TUN_PID" ] && kill -0 "$TUN_PID" && kill $TUN_PID
+            exit 0
 
+        elif [ "$result" == "+" ]
+        then
+            echo -n "Add which host: "
+            read result
+            set -- "$@" "$result"
+        fi
+
+        [ -n "$TUN_PID" ] && kill -0 "$TUN_PID" && kill $TUN_PID && sleep 3
+
+        echo "port $port from host $result will be available locally"
+        ssh -L 8384:localhost:8384 -J jmedin@192.168.10.127 -A root@$result -N &
+        echo tunell started in background
+        sleep 3
+        TUN_PID="$!"
+        
+        new_window="--new-window"
+        if [ -z "$BROWSER_PID" ] || ! kill -0 "$BROWSER_PID"
+        then
+            /snap/bin/chromium $new_window "https://localhost:$port"
+            BROWSER_PID="$(pgrep -f chromium | tail -n 1)"
+        fi
+
+        #wait $TUN_PID
+
+        [ $# -gt 1 ] || exit 0
+
+    done
+}
+
+
+menu()
+{
     declare -A menu
     menu=()
-     menu["q"]="quit"
-     menu["1"]="db:add-missing-indices"
-     menu["2"]="db:convert-filecache-bigint"
-     menu["3"]="app:list"
-     menu["4"]="app:disable"
-[ -n "$conf_f" ] && \
-    menu["ec"]="edit config.php"
-     menu["f"]="groupfolders:scan"
-     menu["s"]="status"
-    menu["fc"]="files:cleanup         - occ files:cleanup"
-    menu["fs"]="files:scan            - occ files:scan --path=path"
-    menu["fa"]="files:scan            - occ files:scan --all"
-     menu["u"]="upgrade apps          - occ upgrade"
-[ -n "$updater_phar" ] && \
-     menu["U"]="upgrade system        - updater/updater.phar"
-     menu["e"]="exit maintenence mode - occ maintenance:mode --off"
-    menu["mr"]="maintenance repair    - occ maintenance:repair --include-expensive"
-    menu["sc"]="setupchecks           - occ setupchecks -vvv"
 
+    while [ $# -gt 0 ]
+    do
+        menu["$1"]="connect to host $1"
+        shift
+    done
+    #[ -n "$TUN_PID" ] && menu["kill"]="kill running tunnel with PID=$TUN_PID"
+    #menu["q"]="quit"
+    #menu["b"]="$BROWSER_PID"
 
     result=""
     if which dialog > /dev/null
@@ -61,66 +88,34 @@ main()
     else
         radiolist="radiolist_txt"
     fi
-    while [ "$result" != q ]
-    do
 
-        result="$($radiolist --title "DIALOG RADIOLIST" --backtitle "A user-built list")"
-        retval="$?"
+    result="$($radiolist --title "DIALOG RADIOLIST" --backtitle "A user-built list" --extra-button --extra-label "Add")"
+    retval="$?"
 
-        case $retval in
-          0)
-            echo "The user chose '$result' - '${menu[$result]}'.";;
-          1)
-            echo "Cancel pressed."
-            result="q"
-            ;;
-          255)
-            echo "Box closed."
-            result="q"
-            ;;
-          *)
-            echo "Unknown retval: $retval"
-            result="q"
-            ;;
-        esac
+    case $retval in
+      0)
+        echo "The user chose '$result' - '${menu[$result]}'.";;
+      1)
+        echo "Cancel pressed."
+        result="q"
+        ;;
+      3)
+        echo "Add pressed."
+        result="+"
+        ;;
+      255)
+        echo "Box closed."
+        result="q"
+        ;;
+      *)
+        echo "Unknown retval: $retval"
+        result="q"
+        echo -n pause
+        read ans
+        ;;
+    esac
 
-        if [ "$result" != "q" ]
-        then
-            echo "press ENTER to continue"
-            read ans
-        fi
-
-        case "$result" in
-            1) /usr/bin/sudo -u $user php $occ db:add-missing-indices | less;;
-            2) /usr/bin/sudo -u $user php $occ db:convert-filecache-bigint | less;;
-            3) /usr/bin/sudo -u $user php $occ app:list | less;;
-            4)
-                echo -n "which app to disable ? "
-                read app
-                /usr/bin/sudo -u $user php $occ app:disable $app | less
-                ;;
-            f)
-                /usr/bin/sudo -u $user php $occ groupfolders:list
-                echo -n "which folder number ? "
-                read num
-                /usr/bin/sudo -u $user php $occ groupfolders:scan $num | less
-                ;;
-            s)  /usr/bin/sudo -u $user php $occ status | less;;
-            sc) /usr/bin/sudo -u $user php $occ setupchecks -vvv | less;;
-            fc) /usr/bin/sudo -u $user php $occ files:ceanup | less;;
-            fs) 
-                echo "example: admin/files/bin"
-                echo -n "which folder to scal ? "
-                read folder
-                /usr/bin/sudo -u $user php $occ files:scan --path="$folder" | less;;
-            fa) /usr/bin/sudo -u $user php $occ files:scan --all | less;;
-            mr) /usr/bin/sudo -u $user php $occ maintenance:repair --include-expensive | less;;
-            u)  /usr/bin/sudo -u $user php $occ upgrade | less;;
-            U)  /usr/bin/sudo -u $user php /var/www/nextcloud/updater/updater.phar | less;; # REF https://docs.nextcloud.com/server/latest/admin_manual/maintenance/update.html
-            e)  /usr/bin/sudo -u $user php $occ maintenance:mode --off; echo "press enter to contunue"; read ans;;
-            ec) vi "$conf_f";;
-        esac
-    done
+    return "$retval"
 }
 
 radiolist_txt()
@@ -145,9 +140,6 @@ radiolist_txt()
     # build the menu
     echo > /dev/stderr
     echo "------" > /dev/stderr
-    echo "user=$user" > /dev/stderr
-    echo "occ=$occ" > /dev/stderr
-    echo  > /dev/stderr
 
     for key in "${i[@]}"
     do
@@ -205,3 +197,4 @@ radiolist()
 
 main "$@"
 exit $?
+
