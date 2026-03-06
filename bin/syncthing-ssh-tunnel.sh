d359a476fa83496450f746e6204419e4e4242e82
Juan Medin <jmedin1965@gmail.com>
2026-03-07 00:21:51 +1100

    fix: now can connect multiple or all at the same time

--- bin/syncthing-ssh-tunnel.sh
+++ bin/syncthing-ssh-tunnel.sh
@@ -10,29 +10,56 @@ conf="$HOME/.$prog.conf"
 host=""
 last_host=""
 
+declare -A port_list
+declare -A pid_list
+next_port="$default_port"
+
+kill_all()
+{
+    local key
+    local PID
+
+    for key in "${!pid_list[@]}"
+    do
+        PID="${pid_list[$key]}"
+        echo "kill host tunnel for \"$key\" with pid $PID"
+        if kill -0 "$PID" &> /dev/null
+        then
+            kill $PID
+        else
+            echo "$key with PID=$PID is not running"
+        fi
+    done
+}
+
 # a function to trap CTRL_C since ssh-add times out or waits forever
 ctrl_c() {
     echo "** Trapped CTRL-C"
-    [ -n "$TUN_PID" ] && kill $TUN_PID
+    kill_all
     echo "msg: trap done"
+    echo hit the ENTER key; read and
+    exit 0
 }
 # trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
 
 main()
 {
+    local PID
+
     if [ ! -e "$conf" ]
     then
         touch "$conf"
     fi
 
     mapfile -t hosts < "$conf"
+    new_window="--new-window"
 
     while :
     do
         if [ "${#hosts[@]}" == "0" ]
         then
-            echo -n "Connect to which host:port or just host, default port is $default_port, or 'q', to quit: "
+            echo -n "Connect to which host:port or just host with default port on $default_port, or 'q', to quit: "
             read host
             if [ "$host" != "q" ]
             then
@@ -45,8 +72,7 @@ main()
 
         if [ "$host" == "q" ]
         then
-            echo $TUN_PID=$TUN_PID
-            [ -n "$TUN_PID" ] && kill -0 "$TUN_PID" && kill $TUN_PID
+            kill_all
             exit 0
 
         elif [ "$host" == "+" ]
@@ -56,7 +82,7 @@ main()
             echo "$host" >> "$conf"
         fi
 
-        [ -n "$TUN_PID" ] && kill -0 "$TUN_PID" && kill $TUN_PID && sleep 3
+        #[ -n "$TUN_PID" ] && kill -0 "$TUN_PID" && kill $TUN_PID && sleep 3
 
         last_host="$host"
         port="${host##*:}"
@@ -67,24 +93,41 @@ main()
         host="${host%%:*}"
         echo host=$host
 
-        echo "port $port from host $host will be available locally"
+        echo "port $port from host $host will be available locally on port $next_port"
         echo "DISPLAY=$DISPLAY"
         echo "conf=$conf"
         pwd
         echo HOME=$HOME
+        echo next_port=$next_port
 
-        ssh -L $default_port:localhost:$port -J jmedin@192.168.10.127 -A root@$host -N &
-        TUN_PID="$!"
+        PID=""
+        [[ -v pid_list[$last_host] ]] && PID="${pid_list[$last_host]}"
+        echo check host $last_host with pid $PID
 
-        echo tunell started in background
-        sleep 3
-        
-        new_window="--new-window"
-        if [ -z "$BROWSER_PID" ] || ! kill -0 "$BROWSER_PID"
+        #echo hit the ENTER key; read and
+
+
+        echo "ssh -4 -L $next_port:localhost:$port -J jmedin@192.168.10.127 -A root@$host -N"
+
+        if [ -n "$PID" ] && kill -0 "$PID" &> /dev/null
         then
-            /snap/bin/chromium $new_window "https://localhost:$default_port"
-            BROWSER_PID="$(pgrep -f chromium | tail -n 1)"
+            echo "Tunnel already open, not opening another."
+        else
+
+            #echo hit the ENTER key; read and
+
+            ssh -4 -L $next_port:localhost:$port -J jmedin@192.168.10.127 -A root@$host -N 2>/dev/null&
+
+            pid_list[$last_host]="$!"
+            port_list[$last_host]="$next_port"
+
+            echo "tunell started in background with pid=${pid_list[$last_host]}"
+            (( next_port++ ))
         fi
+        sleep 4
+        
+        /snap/bin/chromium $new_window "https://localhost:${port_list[$last_host]}" >/dev/null  2>&1 &
+        new_window=""
 
         #wait $TUN_PID
     done
@@ -98,10 +141,14 @@ menu()
 
     while [ $# -gt 0 ]
     do
-        menu["$1"]="connect to host $1"
+        if [ -n "${port_list[$1]}" ]
+        then
+            menu["$1"]="connected on port ${port_list[$1]} with pid=${pid_list[$1]}"
+        else
+            menu["$1"]="connect to host"
+        fi
         shift
     done
-    #[ -n "$TUN_PID" ] && menu["kill"]="kill running tunnel with PID=$TUN_PID"
     menu["q"]="quit"
     #menu["b"]="$BROWSER_PID"
 
@@ -190,13 +237,14 @@ radiolist()
     # get the array keys in array i
     for key in "${!menu[@]}"
     do
-        i+=("$key")
+        [ "$key" != "q" ] && i+=("$key")
     done
 
     # sort the keys
     local ifs="$IFS"
     IFS=$'\n' i=($(sort <<<"${i[*]}"))
     IFS="$ifs"
+    i+=("q")
 
     # build the menu
     for key in "${i[@]}"
@@ -213,7 +261,7 @@ radiolist()
         fi
     done
 
-    result="$(dialog "$@" --stdout --radiolist "ssh tunnel port=$default_port, host=$last_host" 0 0 12 "${m[@]}")"
+    result="$(dialog "$@" --stdout --radiolist "ssh tunnel next_port=$next_port, last_host=$last_host" 0 0 12 "${m[@]}")"
     retval=$?
     echo "$result"
     return "$retval"
